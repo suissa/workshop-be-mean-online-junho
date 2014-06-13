aecbdb3913c909a927ee796d479e6067fe5411f3
Suissa <jnascimento@gmail.com>
2014-06-13 20:46:40 -0300

    Subindo listagemd e cervejas

--- nodejs/express/controllers/beer.js
+++ nodejs/express/controllers/beer.js
@@ -1,6 +1,9 @@
 // requisitando o model de Beer
 var Beer = require('../models/beer');
 
+// Criandoa  variável de retorno de mensagem
+var msg = '';
+
 // criando o objeto com as funcionalidades visuais do sistema
 var _beer = {
   // função que renderizará o form de criação da cerveja
@@ -8,8 +11,36 @@ var _beer = {
   },
   // função que renderizará a index
   index: function(req, res){
-    // Vamos criar a renderização do nosso INDEX
-    res.render('beer/index', {title: 'Adega Be MEAN'});
+    // COmo nossa função é assíncrona precisamos colocar nossa
+    // renderização dentro dela
+    Beer.find({}, function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        msg = 'Erro ao listar as cervejas!';
+        // Vamos criar a renderização do nosso INDEX
+        // Preciso passar o array de cervejas
+        res.render('beer/index', 
+          {
+            title: 'Adega Be MEAN', 
+            msg: msg
+          }
+        );
+      }else{
+        console.log('Listagem: ', data);  
+        msg = 'Listagem das cervejas'; 
+        // Vamos criar a renderização do nosso INDEX
+        // Preciso passar o array de cervejas
+        
+        res.render('beer/index', 
+          {
+            title: 'Adega Be MEAN', 
+            cervejas: data,
+            msg: msg
+          }
+        );
+      }
+   
+    }); 
   },
   // função que renderizará a consulta da cerveja
   show: function(req, res){
