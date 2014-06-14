6d0394bd2a749f2c4402b59b7415a6512fadf00a
Suissa <jnascimento@gmail.com>
2014-06-13 22:45:03 -0300

    update com method override

--- nodejs/express/app.js
+++ nodejs/express/app.js
@@ -30,15 +30,17 @@ app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-// utilizando o middleware que emula PUT e DELETE vindo pelo HTML
-app.use(methodOverride(function(req, res){
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
-  }
-}))
+// utilizando o middleware que emula PUT e DELETE vindo pelo
+app.use(
+    methodOverride(function(req, res){
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+);
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
