6fbdad8a0625c622a1bd0e87bbfa6b444b9d565b
Suissa <jnascimento@gmail.com>
2014-06-13 22:46:48 -0300

    update com method override

--- nodejs/express/app.js
+++ nodejs/express/app.js
@@ -30,14 +30,14 @@ app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-// utilizando o middleware que emula PUT e DELETE vindo pelo
+// utilizando o middleware que emula PUT e DELETE vindo pelo HTML
 app.use(
     methodOverride(function(req, res){
       if (req.body && typeof req.body === 'object' && '_method' in req.body) {
         // look in urlencoded POST bodies and delete it
-        var method = req.body._method
-        delete req.body._method
-        return method
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
       }
     })
 );
--- readme.md
+++ readme.md
@@ -128,7 +128,18 @@ Não esquecer de instalar localmente:
         npm install --save method-override
 
 
+Para usarmos o method override com o nosso input, precisamos chamá-lo:
 
+    app.use(
+        methodOverride(function(req, res){
+          if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            var method = req.body._method;
+            delete req.body._method;
+            return method;
+          }
+        })
+    );
 
 
 
